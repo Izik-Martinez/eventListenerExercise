1e263a83074f9d328a8dc1e797cc0fa9572e6501
Izik-Martinez <izikma17@yahoo.com>
2022-04-12 17:04:29 -0400

    CComment added

--- script.js
+++ script.js
@@ -17,21 +17,35 @@ const allDivs = document.querySelectorAll(".col");
 
 for (let div of allDivs) {
 
-    
 
-div.addEventListener("click", function (){
+  div.addEventListener("click", function (){
 
     for (let div of allDivs) {
 
         div.style.backgroundColor = "grey";
+        div.style.boxShadow = "none"; 
+
     }
 
-this.style.backgroundColor = "black";
+  this.style.backgroundColor = "black";
+  this.style.boxShadow = "0px 0px 2px 2px black"; 
 
 
 
-})
+ })
 
 };
+
+// for (let div of allDivs) {
+
+// div.addEventListener("mouseover", () =>{
+
+//   div.style.height = "300px"
+  
+
+//  })
+
+    
+// }
 
     
