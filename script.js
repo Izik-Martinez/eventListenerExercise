f0f314972574d0fb5234c1946a768833e33728ba
Izik-Martinez <izikma17@yahoo.com>
2022-04-12 16:25:11 -0400

    Now each box that's clicked, resets the background color of the rest before changing background color itself

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 const allDivs = document.querySelectorAll(".col");
 
+
+
+// for (let div of allDivs){
+//     div.addEventListener("click", function (){
+    
+//         allDivs.style.backgroundColor = "green";
+        
+        
+        
+//     })
+        
+//     };
+
+
+
 for (let div of allDivs) {
+
+    
 
 div.addEventListener("click", function (){
 
-  
+    for (let div of allDivs) {
+
+        div.style.backgroundColor = "grey";
+    }
+
 this.style.backgroundColor = "black";
+
 
 
 })
 
-}
+};
+
+    
