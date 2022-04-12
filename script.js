3672c8adc314acc05a46a09d1682163a2f4e470a
Izik-Martinez <izikma17@yahoo.com>
2022-04-12 15:51:09 -0400

    Targeted all divs then iterating over each of them to add Event Listeners. Now each grey block will change color on click

---
+++ script.js
@@ -0,0 +1,13 @@
+const allDivs = document.querySelectorAll(".col");
+
+for (let div of allDivs) {
+
+div.addEventListener("click", function (){
+
+  
+this.style.backgroundColor = "black";
+
+
+})
+
+}
